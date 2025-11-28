ec520aacf77ab6991c84203c1dd7ef91b67711ba
tranmylinhh <tranmylinhh@gmail.com>
2025-11-28 15:08:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown("""
 # --- 3. KẾT NỐI API KEY ---
 if 'GOOGLE_API_KEY' in st.secrets:
     genai.configure(api_key=st.secrets['GOOGLE_API_KEY'])
-    model = genai.GenerativeModel('gemini-2.0-flash-exp')
+    model = genai.GenerativeModel('gemini-2.5-pro-preview-03-25')
 else:
     st.error("⚠️ Lỗi: Chưa kết nối API Key. Vui lòng báo Admin.")
     st.stop()
